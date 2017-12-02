e58bb738ca374021680a7b17450a0080cc82c0cc
bennypeng <303810616@qq.com>
2017-12-02 23:22:36 +0800

    完成主页美化

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,33 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', '主页') - 互联网路上的徒行</title>
+    <title>@yield('title', '主页') - 专注互联网开发</title>
+    <link rel="stylesheet" href="/css/app.css"/>
+    <script type="text/javascript" src="/js/app.js"></script>
   </head>
   <body>
-    @yield('content')
+    <div class="container">
+      <header class="navbar navbar-inverse navbar-fixed-top">
+        <div class="row">
+          <div class="col-md-4">
+            <a id="logo" class="navbar-brand">BP BLOG</a>
+          </div>
+          <div class="col-md-4 col-md-offset-4">
+            <nav>
+              <ul class="nav navbar-nav navbar-left">
+                <li><a href="/">首页</a></li>
+                <li><a href="#">干货</a></li>
+                <li><a href="/music">音乐</a></li>
+                <li><a href="/help">帮助</a></li>
+                <li><a href="/about">关于</a></li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+    </div>
+    <div class="container">
+      @yield('content')
+    </div>
   </body>
 </html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.default')
 @section('title', '主页')
 @section('content')
-  <h1>主页</h1>
+<div class="jumbotron">
+  <p class="lead">
+    你现在看到的是 <a href="#">BP Blog</a> 的展示主页
+  </p>
+  <p>
+    一切将从这里开始...
+  </p>
+  <p>
+    <a class="btn btn-lg btn-info" href="#" role="button">开启新世界</a>
+  </p>
+</div>
 @stop
